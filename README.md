16ff2e724c193d00ab07ffc81d1eae3327d89ad8
Rodrigo Rodrigues <rodrigo.rodrigues@compasso.com.br>
2023-04-09 16:42:48 -0300

    primeiros passos: criação projeto, template, estilos e componentes

--- README.md
+++
@@ -1,2 +0,0 @@
-# Angular-7-8---Udemy
-Angular 7/8 - Material Design + Node.js + MongoDB + Firebase
---
+++ myapp/src/app/myfirst.component.ts
@@ -0,0 +1,18 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "myfirst",
+    // Importação diretiva.
+    //selector: "[myfirst]",
+    // Importação classe.
+    // selector: ".myfirst",
+    templateUrl: "./myfirst.component.html",
+    styleUrls: ["./myfirst.component.css"],
+    // Importação inline.
+    // template: ``,
+    // styles: ``,
+})
+
+export class MyFirstComponent {
+
+}
